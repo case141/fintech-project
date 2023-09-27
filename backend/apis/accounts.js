26a582b3ff03492d93b4d99f830109ace19631e8
Cassey Low <casseyl141@gmail.com>
2023-09-27 21:39:29 +0800

    finish api for accounts

--- backend/apis/accounts.js
+++ backend/apis/accounts.js
@@ -15,4 +15,52 @@ router.get("/get-all", (request, response) => {
   });
 });
 
+// Get Account by Account Number
+router.get("/get-account-by-account-number", (request, response) => {
+    database.mysqlConnection.query(`SELECT * FROM accounts WHERE account_number = ${request.query.account_number}`, (error, results) => {
+        if (error) {
+        console.error(error);
+        response.status(500).send("Internal Server Error");
+        } else {
+        response.status(200).send(results);
+        }
+    });
+});
+
+// Create Account
+router.post("/create-account", (request, response) => {
+    database.mysqlConnection.query(`INSERT INTO accounts (account_number, type, balance, creation_date) VALUES ('${request.body.account_number}','${request.body.type}','${request.body.balance}', now())`, (error, results) => {
+        if (error) {
+        console.error(error);
+        response.status(500).send("Internal Server Error");
+        } else {
+        response.status(200).send(results);
+        }
+    });
+});
+
+// Update Account Balance by Account Number
+router.put("/update-balance-by-account-number", (request, response) => {
+    database.mysqlConnection.query(`UPDATE accounts SET balance = ${request.body.balance} WHERE account_number = ${request.body.account_number}`, (error, results) => {
+        if (error) {
+        console.error(error);
+        response.status(500).send("Internal Server Error");
+        } else {
+        response.status(200).send(results);
+        }
+    });
+});
+
+// Delete Account by Account Number
+router.delete("/delete-account-by-account-number", (request, response) => {
+    database.mysqlConnection.query(`delete from accounts where account_number= ${request.query.account_number}`, (error, results) => {
+        if (error) {
+        console.error(error);
+        response.status(500).send("Internal Server Error");
+        } else {
+        response.status(200).send(results);
+        }
+    });
+});
+
 module.exports = { router };
